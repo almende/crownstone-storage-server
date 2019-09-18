f736eaa69c5339b7ef68714aef856de7498762f7
Ludo Stellingwerff <ludo@almende.org>
2019-09-18 16:19:37 +0200

    First code base

--- index.js
+++ index.js
@@ -1,5 +1,84 @@
 // Set options as a parameter, environment variable, or rc file.
 // eslint-disable-next-line no-global-assign
-require = require("esm")(module/* , options */)
+require = require('esm')(module/* , options */)
+const Express = require('express')
+const Influx = require('influx')
 
-module.exports = require("./main.js")
+const argv = require('minimist')(process.argv.slice(2), {
+  'alias': { 'port': 'p', 'db_hostname': 'h', 'db_name': 'd' },
+  'default': { 'port': 3000, 'db_hostname': 'localhost', 'db_name': 'crownstone' }
+})
+
+// Starting Application information block:
+console.log('Starting application:')
+console.log('Used command-line options:' + JSON.stringify(argv))
+
+// Input validation:
+if (!parseInt(argv['port'])) {
+  console.error('Invalid portnumber given:' + argv['port'])
+  process.exit()
+}
+
+//Init db access:
+const influx = new Influx.InfluxDB({
+  host: argv['db_hostname'],
+  database: argv['db_name']
+})
+
+//All is well, starting:
+const app = Express()
+const api = new Express()
+app.use(Express.static('static'))
+app.use(require('body-parser').json())
+
+api
+  .post('/', (req, res) => {
+    res.setHeader('Content-Type', 'application/json')
+    if (!req.is('application/json')) {
+      // Send error here
+      res.status(400).send({ 'result': 'error', 'error': 'Expected application/json contents' })
+    } else {
+      let measurement = req.body
+      if (!measurement['MAC Address'] || !measurement['Local Timestamp']) {
+        res.status(400).send({
+          'result': 'error',
+          'error': 'This doesn\'t look like Crownstone data to me, missing MAC Address or Timestamp'
+        })
+      } else {
+        //forward measurement to influx db
+        influx.writePoints([{
+          measurement: 'crownstone',
+          tags: {
+            macaddress: measurement['MAC Address'],
+            devicename: measurement['Device Name'],
+            devicetype: measurement['Device Type'],
+            datatype: measurement['Data Type'],
+            crownID: measurement['Crown ID'],
+            switchstate: measurement['Switch State'],
+            flags: measurement['Flags']
+          },
+          fields: {
+            temperature: measurement['Temperature'],
+            powerfactor: measurement['Power Factor'],
+            powerusage: measurement['Power Usage'],
+            energyused: measurement['Energy Used']
+          },
+          timestamp: measurement['Local Timestamp']
+        }], { precision: 's' })
+          .then(() => {
+            let result = { 'result': 'ok' }
+            res.send(result)
+          }).catch((e) => {
+          console.error(e)
+          res.status(500).send({ 'result': 'error', 'error': 'Couldn\'t write to database' })
+        })
+      }
+    }
+  })
+app.use('/api/', api)
+
+app.listen(argv['port'], () => {
+  console.log('Server running on port ' + argv['port'])
+})
+
+module.exports = require('./main.js')
